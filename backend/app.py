8b8559a5905a707d730bca99dbba0d8a89da1651
Apurv Chandel <48324648+apurvchandel@users.noreply.github.com>
2026-02-02 04:25:24 +0530

    backend api fix

--- backend/app.py
+++ backend/app.py
@@ -40,4 +40,5 @@ def home():
     return 'PDF Unlocker Backend is running!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
